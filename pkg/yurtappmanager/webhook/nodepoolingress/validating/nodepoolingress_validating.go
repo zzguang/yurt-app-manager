7c2026e49fd0c4732c71ed2a2b08883344ad36e7
zhenggu1 <zhengguang.zhang@intel.com>
2021-12-16 22:27:53 +0800

    Revert "validating webhook for nodepool ingress controller"
    This reverts commit 122e312fc4648709e99ce8f336daeade963e846d.

--- pkg/yurtappmanager/webhook/nodepoolingress/validating/nodepoolingress_validating.go
+++ pkg/yurtappmanager/webhook/nodepoolingress/validating/nodepoolingress_validating.go
@@ -17,57 +17,21 @@ limitations under the License.
 package validating
 
 import (
-	"context"
-
 	"k8s.io/apimachinery/pkg/util/validation/field"
-	"k8s.io/klog"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	appsv1alpha1 "github.com/openyurtio/yurt-app-manager/pkg/yurtappmanager/apis/apps/v1alpha1"
 )
 
 // validateNodePoolIngressSpec validates the nodepool ingress spec.
-func validateNodePoolIngressSpec(c client.Client, spec *appsv1alpha1.NodePoolIngressSpec) field.ErrorList {
-
-	if len(spec.Pools) > 0 {
-		var err error
-		var errmsg string
-		nps := appsv1alpha1.NodePoolList{}
-		if err = c.List(context.TODO(), &nps, &client.ListOptions{}); err != nil {
-			errmsg = "List nodepool list error!"
-			klog.Errorf(errmsg)
-			return field.ErrorList([]*field.Error{
-				field.Forbidden(field.NewPath("spec").Child("pools"), errmsg)})
-		}
-
-		// validate whether the nodepool exist
-		var found = false
-		if len(nps.Items) > 0 {
-			for _, snp := range spec.Pools { //go through the nodepools setting in yaml
-				for _, cnp := range nps.Items { //go through the nodepools in cluster
-					if snp == cnp.ObjectMeta.Name {
-						found = true
-						break
-					}
-				}
-				if !found {
-					errmsg = snp + " does not exist in the cluster!"
-					klog.Errorf(errmsg)
-					return field.ErrorList([]*field.Error{
-						field.Forbidden(field.NewPath("spec").Child("pools"), errmsg)})
-				}
-				found = false
-			}
-
-		}
-	}
+func validateNodePoolIngressSpec(spec *appsv1alpha1.NodePoolIngressSpec) field.ErrorList {
 	return nil
 }
 
-func validateNodePoolIngressSpecUpdate(c client.Client, spec, oldSpec *appsv1alpha1.NodePoolIngressSpec) field.ErrorList {
-	return validateNodePoolIngressSpec(c, spec)
-}
+// validateNodePoolIngressSpecUpdate tests if required fields in the NodePoolIngress spec are set.
+func validateNodePoolIngressSpecUpdate(spec, oldSpec *appsv1alpha1.NodePoolIngressSpec) field.ErrorList {
+	if allErrs := validateNodePoolIngressSpec(spec); allErrs != nil {
+		return allErrs
+	}
 
-func validateNodePoolIngressDeletion(c client.Client, spec *appsv1alpha1.NodePoolIngressSpec) field.ErrorList {
-	return validateNodePoolIngressSpec(c, spec)
+	return nil
 }
